17552648db17a4bf21c250c46c96d40e003d3412
e1b20092 <e1b20092@st.oit.ac.jp>
2022-10-10 17:03:37 +0900

    jankengameの実装

--- src/main/java/oit/is/z1406/kaizi/janken/controller/JankenController.java
+++ src/main/java/oit/is/z1406/kaizi/janken/controller/JankenController.java
@@ -16,8 +16,35 @@ public class JankenController {
   }
 
   @PostMapping("/Janken")
-  public String sample(@RequestParam String userName,ModelMap model) {
+  public String sample(@RequestParam String userName, ModelMap model) {
     model.addAttribute("userName", userName);
+    return "janken.html";
+  }
+
+  @GetMapping("/gu")
+  public String sample2(ModelMap model) {
+    String gu = "Gu";
+    String draw = "You Draw";
+    model.addAttribute("te", gu);
+    model.addAttribute("result", draw);
+    return "janken.html";
+  }
+
+  @GetMapping("/cho")
+  public String sample3(ModelMap model) {
+    String cho = "Choki";
+    String lose = "You Lose";
+    model.addAttribute("te", cho);
+    model.addAttribute("result", lose);
+    return "janken.html";
+  }
+
+  @GetMapping("/pa")
+  public String sample4(ModelMap model) {
+    String pa = "Pa";
+    String win = "You Win";
+    model.addAttribute("te", pa);
+    model.addAttribute("result", win);
     return "janken.html";
   }
 }
